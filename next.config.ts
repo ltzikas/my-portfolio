a52209245c3bfb2b1168a93a1671e7feaa8faebd
ltzikas <39968493+ltzikas@users.noreply.github.com>
2025-05-17 13:00:45 -0300

    Prisma generate

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  ignoreduringBuilds: true,
+  /* config options here */
 };
 
 export default nextConfig;
--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -154,7 +154,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
